63629ab22ecb4e3bff0748216135c8a4312e7b47
Alexandr Vasildzhagaz <dzhagaz.sasha@gmail.com>
2016-10-24 22:28:16 +0300

    Change badge animation type configuration ability in tutorial app

--- ButtonWithBadge/ButtonWithBadge/ViewController.swift
+++ ButtonWithBadge/ButtonWithBadge/ViewController.swift
@@ -3,7 +3,7 @@
 //  ButtonWithBadge
 //
 //  Created by Alexandr on 9/23/16.
-//  Copyright © 2016 v-damki-man. All rights reserved.
+//  Copyright © 2016 dzhagazs. All rights reserved.
 //
 
 import UIKit
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var tapButton: ButtonWithBadge!
+	@IBOutlet weak var animationTypeSegmentedControl: UISegmentedControl!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tapButton.badgeValue = 10
+		animationTypeSegmentedControl.selectedSegmentIndex = tapButton.animationType.rawValue
 	}
 	
 	// MARK: - Actoins
@@ -34,6 +36,7 @@ class ViewController: UIViewController {
 		
 		tapButton.badgePosition = nextPosition
 	}
+	
 	@IBAction func plusAction(sender: AnyObject) {
 		tapButton.badgeValue += 10
 	}
@@ -44,6 +47,10 @@ class ViewController: UIViewController {
 	
 	@IBAction func badgeSIzeValueChange(sender: UISlider) {
 		tapButton.badgeFontSize = CGFloat(sender.value)
+	}
+	
+	@IBAction func changeAnimationType(sender: UISegmentedControl) {
+		tapButton.animationType = BadgeAnimationType(rawValue: sender.selectedSegmentIndex)!
 	}
 }
 
